b72792c810f91ba24ca7e4e9a4f695e89c883551
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-01 21:38:16 -0500

    updated server

--- app.js
+++ app.js
@@ -12,19 +12,13 @@ const port = 8080
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/users', users)
+app.use('/api/users', users)
 
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  )
-  next()
+app.get('/', (req, res) => {
+  res.send('hello world')
 })
 
-var server = require('https').createServer(app)
-
-server.listen(port, () => console.log('Listening on port ' + port))
+app.listen(port)
+console.log('Server Started')
 
 module.exports = app
