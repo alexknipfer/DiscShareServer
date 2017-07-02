d2a2f63317719b667b20ab00d2743d7a5e032528
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-01 21:21:31 -0500

    server update

--- app.js
+++ app.js
@@ -14,6 +14,15 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/users', users)
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
+
 var server = require('https').createServer(app)
 
 server.listen(port, () => console.log('Listening on port ' + port))
