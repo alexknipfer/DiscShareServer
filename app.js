e7763bbc2070a832747cf96977e90d5c1903b953
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-02 18:02:07 -0500

    setup mongoose and user model

--- app.js
+++ app.js
@@ -2,23 +2,30 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const config = require('./config')
+
+mongoose.connect(config.DB_CONNECTION_STRING, {
+  useMongoClient: true
+})
+
+let db = mongoose.connection
+
+db.once('open', () => {
+  console.log('Connected to MongoDB')
+})
+
+db.on('error', err => {
+  console.log(err)
+})
 
 var users = require('./routes/users')
 
 var app = express()
 
-const port = 8080
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use('/api/users', users)
-
-app.get('/', (req, res) => {
-  res.send('hello world')
-})
-
-app.listen(port)
-console.log('Server Started')
 
 module.exports = app
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,15 @@
 var express = require('express')
 var router = express.Router()
+const UserModel = require('../db/models/UserModel')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  //res.send('respond with a resource');
-
-  res.json({
+  //console.log('hello')
+  UserModel.find((err, users) => console.log(users))
+  /*res.json({
     id: 1,
     username: 'hello there'
-  })
+  })*/
 })
 
 module.exports = router
