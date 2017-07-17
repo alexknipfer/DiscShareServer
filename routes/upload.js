5a110692bf5ac30e923a41cf7997b1be430540c2
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-16 20:08:58 -0500

    use multer

--- routes/upload.js
+++ routes/upload.js
@@ -1,14 +1,12 @@
 const express = require('express')
+const multer = require('multer')
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 const router = express.Router()
 
-router.post('/uploadProfileImage', (req, res, next) => {
-  const { files } = req.body
-  const file = files[0]
-
-  console.log(file)
-
-  res.send({ file })
+router.post('/uploadProfileImage', (req, res) => {
+  console.log(req.files)
 })
 
 module.exports = router
