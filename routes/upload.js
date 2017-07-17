0cec605172cdd5a4b10247f87d76f5ecf47f27a9
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-16 21:16:52 -0500

    fixes

--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,11 @@
 const express = require('express')
+const router = express.Router()
 const multer = require('multer')
 const storage = multer.memoryStorage()
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage, limits: { fileSize: '4MB' } })
 
-const router = express.Router()
-
-router.post('/uploadProfileImage', (req, res) => {
-  console.log(req.files)
+router.post('/uploadProfileImage', upload.single('avatar'), (req, res) => {
+  console.log(req.file)
 })
 
 module.exports = router
