da9bce1b3c0941275238075c0a57641cd19ed86a
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-17 22:58:12 -0500

    added register mutation

--- schemas/index.js
+++ schemas/index.js
@@ -1,24 +1,37 @@
 const { makeExecutableSchema } = require('graphql-tools')
+const { merge } = require('lodash')
 const testSchema = require('./testSchema')
+const register = require('./register')
 
 const rootSchema = `
   type Query {
     version: String!
   }
+  type Mutation {
+    version: String!
+  }
   schema {
     query: Query
+    mutation: Mutation
   }
 `
 
 const rootResolvers = {
   Query: {
     version: () => require('../package.json').version
+  },
+  Mutation: {
+    version: () => require('../package.json').version
   }
 }
 
 const schema = makeExecutableSchema({
-  typeDefs: [rootSchema, testSchema.testTypeDef],
-  resolvers: testSchema.testResolver
+  typeDefs: [rootSchema, testSchema.testTypeDef, register.registerTypeDef],
+  resolvers: merge(
+    rootResolvers,
+    testSchema.testResolver,
+    register.registerResolver
+  )
 })
 
 module.exports = schema
---
+++ schemas/register.js
@@ -0,0 +1,43 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const config = require('../config')
+
+const registerTypeDef = `
+  extend type Mutation {
+    register(email: String!, username: String!, password: String!): String
+  }
+`
+
+const registerResolver = {
+  Mutation: {
+    register: async ({ db }, { email, username, password }) => {
+      const userCollection = db.collection('users')
+      const duplicateUser = await userCollection
+        .find({ username: username })
+        .count()
+
+      if (duplicateUser >= 1) return 'User exists'
+      else {
+        const saltRounds = 10
+        const hash = bcrypt.hashSync(password, saltRounds)
+
+        const data = {
+          email,
+          username,
+          password: hash
+        }
+        await userCollection.insertOne(data)
+        const newUser = await userCollection.findOne({ username: username })
+        const token = jwt.sign(newUser, config.JWT_SECRET, {
+          expiresIn: 60 * 60 * 24
+        })
+        return token
+      }
+    }
+  }
+}
+
+module.exports = {
+  registerTypeDef,
+  registerResolver
+}
