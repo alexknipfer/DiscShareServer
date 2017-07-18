00ab50129a24776ca4257618287ddb8356c37dda
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-07-17 22:58:02 -0500

    add database connection to server

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const graphqlHTTP = require('express-graphql')
 const bodyParser = require('body-parser')
 const schema = require('./schemas/index')
 const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+const config = require('./config')
 
 const app = express()
 
@@ -11,13 +13,27 @@ const port = 4000
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
+
 app.use(
   '/graphql',
-  graphqlHTTP({
+  graphqlHTTP(req => ({
     schema: schema,
+    rootValue: {
+      db: req.app.locals.db
+    },
     graphiql: true
-  })
+  }))
 )
 
-app.listen(port)
-console.log(`Running a GraphQL API server at ${port}`)
+MongoClient.connect(config.DB_CONNECTION_STRING, {
+  promiseLibrary: Promise
+})
+  .catch(err => console.error(err.stack))
+  .then(db => {
+    app.locals.db = db
+    console.log('Database connection successful.')
+    app.listen(port, err => {
+      if (err) throw err
+      console.log(`Running a GraphQL API server at ${port}`)
+    })
+  })
