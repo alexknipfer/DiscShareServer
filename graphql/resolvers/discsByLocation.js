106e3c5f0e660c2d4361387ec5f5458371625753
alexknipfer <alexanderknipfer@gmail.com>
2017-09-25 21:14:12 -0500

    Format

--- graphql/resolvers/discsByLocation.js
+++ graphql/resolvers/discsByLocation.js
@@ -2,19 +2,22 @@ const discsByLocationResolver = {
   Query: {
     discsByLocation: async ({ db }, { longitude, latitude }) => {
       await db.collection('discs').createIndex({ location: '2dsphere' })
-      
-      return await db.collection('discs').find({
-        location: {
-          $near: {
-            $geometry: {
-              type: 'Point', 
-              coordinates: [parseFloat(longitude), parseFloat(latitude)]
-            },
-            $minDistance: 0,
-            $maxDistance: 100
+
+      return await db
+        .collection('discs')
+        .find({
+          location: {
+            $near: {
+              $geometry: {
+                type: 'Point',
+                coordinates: [parseFloat(longitude), parseFloat(latitude)]
+              },
+              $minDistance: 0,
+              $maxDistance: 100
+            }
           }
-        }
-      }).toArray()
+        })
+        .toArray()
     }
   }
 }
