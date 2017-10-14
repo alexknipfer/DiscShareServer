c41cc7928bcbfc8d406b718499eba4b9b06cb03c
alexknipfer <alexanderknipfer@gmail.com>
2017-10-14 15:51:20 -0500

    Completed password reset

--- api/mutations/resetPassword.js
+++ api/mutations/resetPassword.js
@@ -2,11 +2,45 @@ const jwt = require('jsonwebtoken')
 const ObjectId = require('mongodb').ObjectId
 const cuid = require('cuid')
 const moment = require('moment')
+const bcrypt = require('bcrypt')
 
 const resetPassword = async ({ db }, { password, token }) => {
   const userCollection = db.collection('users')
-  console.log('password: ', password)
-  console.log('token: ', token)
+
+  const user = await userCollection.findOne({
+    passwordResetToken: token
+  })
+
+  if (!user) {
+    throw new Error('Password reset link is invalid.')
+  }
+
+  const timeOffset = moment().isAfter(user.passwordResetExpiration)
+
+  if (timeOffset) {
+    throw new Error('Password reset link is invalid or expired.')
+  }
+
+  const saltRounds = 10
+  const hash = bcrypt.hashSync(password, saltRounds)
+
+  await userCollection.updateOne(
+    { passwordResetToken: token },
+    {
+      $set: {
+        password: hash,
+        passwordResetToken: null,
+        passwordResetExpiration: null
+      }
+    }
+  )
+
+  const updatedUser = await userCollection.findOne({ username: user.username })
+  const jwtToken = jwt.sign(updatedUser, process.env.JWT_SECRET, {
+    expiresIn: 60 * 60 * 24
+  })
+
+  return jwtToken
 }
 
 module.exports = resetPassword
