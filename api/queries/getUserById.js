8ea2a5e4732a89dd9bf5b59321f51e7e3a8185b8
alexknipfer <alexanderknipfer@gmail.com>
2017-10-18 21:37:43 -0500

    Cleanup

--- api/queries/getUserById.js
+++ api/queries/getUserById.js
@@ -1,11 +1,7 @@
 const ObjectId = require('mongodb').ObjectId
 
 const getUserById = async ({ db }, { userId }) => {
-  const user = await db
-    .collection('users')
-    .findOne({ _id: new ObjectId(userId) })
-
-  return user
+  return await db.collection('users').findOne({ _id: new ObjectId(userId) })
 }
 
 module.exports = getUserById
