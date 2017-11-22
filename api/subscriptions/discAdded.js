499fdec7444db08fa5770b4bd9e07f732aabb7a6
Alex Knipfer <cr33pyxjtag@yahoo.com>
2017-11-21 22:57:43 -0600

    cleanup

--- api/subscriptions/discAdded.js
+++ api/subscriptions/discAdded.js
@@ -5,7 +5,6 @@ const discAdded = () => {
   subscribe: withFilter(
     () => socket.asyncIterator('DISC_ADDED'),
     (payload, variables) => {
-      console.log('payload: ', payload)
       return payload.createdBy === variables.createdBy
     }
   )
